f7eda0398fde0da039cbef928bf6684bb4b79654
Nathan Tran <natran411@gmail.com>
2022-04-24 10:56:53 -0400

    added routes for navbar

--- server.js
+++ server.js
@@ -7,3 +7,13 @@ app.use(express.static(__dirname + "/public"));
 app.listen(3000, function () {
     console.log("server started at 3000");
 });
+
+app.get("/", function (req, res) {
+    res.sendFile("/public/index.html");
+    console.log("Loading Home Page")
+});
+
+app.get("/events", function (req, res) {
+    res.sendFile(__dirname + "/public/events.html");
+    console.log("Loading Events Page")
+});
