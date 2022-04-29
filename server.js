758265d4f694580c71cb660e1c0b8ad677a70036
Hanvit Choi <66708148+choihan2425@users.noreply.github.com>
2022-04-29 09:04:20 -0400

    adding about_nec page

--- server.js
+++ server.js
@@ -47,15 +47,12 @@ app.get("/people", function (req, res) {
     console.log("Loading People Page")
 })
 
-app.get("/network", function (req, res) {
-    res.sendFile(__dirname + "/public/network.html");
-    console.log("Loading Events Page")
-});
+app.get("/about_nec", function (req, res) {
+    res.sendFile(__dirname + "/public/about_nec.html");
+    console.log("Loading About NEC Page")
+})
 
-app.get("/newsletter", function (req, res) {
-    res.sendFile(__dirname + "/public/newsletter.html");
-    console.log("Loading Events Page")
-});
+
 app.get("/get_all_events", function(req, res){
     Event.find(function (err, data) {
         if (err) {
@@ -105,19 +102,3 @@ app.get('/get_all_people', function (req, res) {
     })
 })
 
-app.get('/get_person_by_id', function (req, res) {
-    // console.log(req.query.movie_id);
-    Person.find({"_id": req.query.person_id}, function (err, data) {
-        if (err || data.length === 0) {
-            res.send({
-                "message": "internal database error",
-                "data": {}
-            });
-        } else {
-            res.send({
-                "message": "success",
-                "data": data[0]
-            })
-        }
-    });
-});
