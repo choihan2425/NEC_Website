5159c1f22b04c54980e8204bf4c74c6c6b066d2b
Nathan Tran <natran411@gmail.com>
2022-05-01 16:41:45 -0400

    added login pages

---
+++ public/js/register.js
@@ -0,0 +1,38 @@
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.get("error")) {
+    $('#error_msg').text(urlParams.get("error"));
+}
+
+$('form').on('submit', function () {
+    const email = $('#email').val()
+    const p1 = $('#password').val()
+    const p2 = $('#confirm').val()
+    const name = $('#fullname').val()
+    const brand = $('#brand').val()
+    if (email === "") {
+        $('#error_msg').text("Email cannot be empty");
+        return false
+    }
+    if (p1.length === 0) {
+        $('#error_msg').text("Password cannot be empty");
+        return false
+    }
+    if (p1.length < 5) {
+        $('#error_msg').text("Password must be at least 5 characters");
+        return false
+    }
+    if (p2.length <= 0){
+        $('#error_msg').text("Confirm Password cannot be empty");
+        return false
+    }
+    if (name.length <= 0){
+        $('#error_msg').text("Fullname cannot be empty");
+        return false
+    }
+
+    if (p1 !== p2) {
+        $('#error_msg').text("passwords do not match");
+        return false
+    }
+});
+
